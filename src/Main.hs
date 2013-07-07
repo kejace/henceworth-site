26d5501ddd172fedeac2c2f6c8537048603754b2
kejace <kejace@gmail.com>
2013-07-07 09:50:48 +0100

    Changed main again

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Network.Wai
 import Network.Wai.Handler.Warp (run)
 
 application _ = return $
-  responseLBS status200 [("Content-Type", "text/plain")] "Hello Nadja.\n\nThis is Kristoffer with Haskell on Heroku."
+  responseLBS status200 [("Content-Type", "text/plain")] "Hello++ Nadja.\n\nThis is Kristoffer with Haskell on Heroku."
 
 main = do
   port <- getEnv "PORT"
